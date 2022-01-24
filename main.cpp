abfc37956e3c7cec13aa7d06135018b1cdc004fc
Alva Melkerson <alva.melkerson@gmail.com>
2022-01-24 15:18:40 -0600

    Slightly less disgusting logic, more run options

--- main.cpp
+++ main.cpp
@@ -2,17 +2,18 @@
 #include <time.h>
 #include <math.h>
 #include <iostream>
-#include <algorithm>
 #include <string>
+
 static const int sec_per_day = 86400;
 static const double magic_julian_offset = 2440587.5;
 static const double known_new_moon = 2415020.5;
 static const double synodic_month = 29.53059;
 static const double phase_length = synodic_month / 8;
+
 time_t t;
-double julian_date;
-double moon_age;
-std::string moon_phase;
+double julian_date, moon_age, next_phase_age, foo;
+int normalized_age;
+std::string moon_phase, next_moon_phase;
 
 double get_moon_age(double datej) {
 	return std::fmod((datej - known_new_moon), synodic_month);
@@ -22,41 +23,59 @@ double get_julian_date(time_t date) {
 	return (date / sec_per_day) + magic_julian_offset;
 }
 
-std::string get_moon_phase(double moonage) {
-	if ( moonage <= phase_length * 1 ) {
-		return "New Moon";
-	} else if (moonage <= phase_length * 2) {
-		return "Waxing Crescent";
-	} else if (moonage <= phase_length * 3 ) {
-		return "First Quarter";
-	} else if (moonage <= phase_length * 4) {
-		return "Waxing Gibbous";
-	} else if (moonage <= phase_length * 5) {
-		return "Full Moon";
-	} else if (moonage <= phase_length * 6) {
-		return "Waning Gibbous";
-	} else if (moonage <= phase_length * 7) {
-		return "Last Quarter";
-	} else if (moonage <= phase_length * 8) {
-		return "Waning Crescent";
-	} else {
-		return "New Moon";
+int normalize_moon_age (double moonage) {
+	int normalized_age;
+	normalized_age = (moonage / phase_length) + 1;
+	return normalized_age;
+}
+
+double get_moon_age_from_normal(int normal_moonage) {
+	return normal_moonage * phase_length;
+}
+
+std::string get_moon_phase_from_normal(int normal_moonage) {
+	switch (normal_moonage) {
+		case 1:
+			return "New Moon";
+		case 2:
+			return "Waxing Crescent";
+		case 3:
+			return "First Quarter";
+		case 4:
+			return "Waxing Gibbous";
+		case 5:
+			return "Full Moon";
+		case 6:
+			return "Waning Gibbous";
+		case 7:
+			return "Last Quarter";
+		case 8:
+			return "Waning Crescent";
+		default:
+			return "New Moon";
 	}
 }
 
 void parseArguments(int argc, char** argv) {
 	std::string arg1 = argv[1];
+	julian_date = get_julian_date(t);
+	moon_age = get_moon_age(julian_date);
+	normalized_age = normalize_moon_age(moon_age);
+	moon_phase = get_moon_phase_from_normal(normalized_age);
+	next_moon_phase = get_moon_phase_from_normal(normalized_age + 1 );
+	next_phase_age = get_moon_age_from_normal(normalized_age);
+	foo = next_phase_age - moon_age;
+
 	if (arg1.compare("age")== 0) {
-		moon_age = get_moon_age(get_julian_date(t));
 		PRINT(moon_age);
 	} else if (arg1.compare("phase")==0) {
-		julian_date = get_julian_date(t);
-		moon_age = get_moon_age(julian_date);
-		moon_phase = get_moon_phase(moon_age);
 		PRINT(moon_phase);
 	} else if (arg1.compare("julian")==0) {
-		julian_date = get_julian_date(t);
 		PRINT(julian_date);
+	} else if (arg1.compare("nextphase")==0) {
+		PRINT(moon_phase);
+	} else if (arg1.compare("test")==0) {
+		PRINT(next_moon_phase << " in " << foo << " days");
 	}
 }
 
@@ -66,9 +85,11 @@ int main(int argc, char** argv) {
 		PRINT("Usage:\n"
 				<< "moonphase age\t\tPrint the moon's age\n"
 				<< "moonphase phase\t\tPrint the moon's phase\n"
-				<< "moonphase julian\tPrint the Julian Date");
+				<< "moonphase julian\tPrint the Julian Date\n"
+				<< "moonphase nextphase\tPrint the next moon's phase, and how long until it occurs.");
 	} else if (argc >= 2) {
 		parseArguments(argc, argv);
 	}
+
 	return 0;
 }
