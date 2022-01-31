de63db19ddccc4fcd2f11145a961bd7aa291c76e
Alva Melkerson <alva.melkerson@gmail.com>
2022-01-31 11:57:04 -0600

    big formatting + prep work for finding dates of phases, intending to build support for outputting a table

--- main.cpp
+++ main.cpp
@@ -4,19 +4,33 @@
 #include <iostream>
 #include <string>
 
-static const int sec_per_day = 86400;
-static const double magic_julian_offset = 2440587.5;
-static const double known_new_moon = 2415020.5;
-static const double synodic_month = 29.53059;
-static const double phase_length = synodic_month / 8;
+static const int sec_per_day			= 86400;
+static const double magic_julian_offset	= 2440587.5;
+static const double known_new_moon		= 2415020.5;
+static const double synodic_month		= 29.53059;
+static const double phase_length		= synodic_month / 8;
 
-time_t t;
-double julian_date, moon_age, next_phase_age, foo;
+time_t t, bar;
+double julian_date, moon_age, next_phase_age, next_phase_days;
 int normalized_age;
-std::string moon_phase, next_moon_phase;
+std::string moon_phase, next_moon_phase, final_output;
+
+std::string get_formatted_date(time_t date) {
+//	return asctime(localtime(&date));
+	struct tm * timeinfo;
+	char buffer [80];
+
+	timeinfo = localtime(&date);
+	strftime(buffer, 80, "%a %b %d %g", timeinfo);
+	return buffer;
+}
 
 double get_moon_age(double datej) {
 	return std::fmod((datej - known_new_moon), synodic_month);
+}
+
+time_t get_unix_time(double datej) {
+	return ((datej + 1 ) - magic_julian_offset) * sec_per_day;
 }
 
 double get_julian_date(time_t date) {
@@ -57,36 +71,44 @@ std::string get_moon_phase_from_normal(int normal_moonage) {
 }
 
 void parseArguments(int argc, char** argv) {
-	std::string arg1 = argv[1];
-	julian_date = get_julian_date(t);
-	moon_age = get_moon_age(julian_date);
-	normalized_age = normalize_moon_age(moon_age);
-	moon_phase = get_moon_phase_from_normal(normalized_age);
-	next_moon_phase = get_moon_phase_from_normal(normalized_age + 1 );
-	next_phase_age = get_moon_age_from_normal(normalized_age);
-	foo = next_phase_age - moon_age;
+	std::string arg1 	= argv[1];
+	julian_date 		= get_julian_date(t);
+	bar					= get_unix_time(julian_date);
+	moon_age			= get_moon_age(julian_date);
+	normalized_age		= normalize_moon_age(moon_age);
+	moon_phase			= get_moon_phase_from_normal(normalized_age);
+	next_moon_phase		= get_moon_phase_from_normal(normalized_age + 1);
+	next_phase_age		= get_moon_age_from_normal(normalized_age);
+	next_phase_days		= next_phase_age - moon_age;
 
-	if (arg1.compare("age")== 0) {
-		PRINT(moon_age);
-	} else if (arg1.compare("phase")==0) {
-		PRINT(moon_phase);
-	} else if (arg1.compare("julian")==0) {
-		PRINT(julian_date);
-	} else if (arg1.compare("nextphase")==0) {
-		PRINT(moon_phase);
-	} else if (arg1.compare("test")==0) {
-		PRINT(next_moon_phase << " in " << foo << " days");
+	if (arg1.compare("age")						== 0 ) {
+		final_output = std::to_string(moon_age);
+	} else if (arg1.compare("phase")			== 0 ) {
+		final_output = moon_phase;
+	} else if (arg1.compare("julian")			== 0 ) {
+		final_output = std::to_string(julian_date);
+	} else if (arg1.compare("nextphase")		== 0 ) {
+		final_output = next_moon_phase + 
+			" in " + 
+			std::to_string(next_phase_days) + 
+			" days";
+	} else if (arg1.compare("nextphasedate")	== 0 ) {
+		final_output = "Formatted: " + 
+			get_formatted_date(t + (next_phase_days * sec_per_day));
 	}
+	PRINT(final_output);
 }
 
 int main(int argc, char** argv) {
 	t = time(NULL);
+
 	if (argc == 1) {
 		PRINT("Usage:\n"
 				<< "moonphase age\t\tPrint the moon's age\n"
 				<< "moonphase phase\t\tPrint the moon's phase\n"
 				<< "moonphase julian\tPrint the Julian Date\n"
-				<< "moonphase nextphase\tPrint the next moon's phase, and how long until it occurs.");
+				<< "moonphase nextphase\tPrint the next moon's phase, and how long until it occurs.\n"
+				<< "moonphase nextphasedate\tPrint the next moon phase's date.");
 	} else if (argc >= 2) {
 		parseArguments(argc, argv);
 	}
